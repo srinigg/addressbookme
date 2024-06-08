4fb02d8d459fb1ab3795030db05ec8be4fa61144
Srinivasan GG <speaktogg@gmail.com>
2024-06-08 09:55:16 +0530

    webserver module

--- terraform/modules/webserver/main.tf
+++ terraform/modules/webserver/main.tf
@@ -1,72 +1,51 @@
-data "aws_ami" "my-ami" {
-  most_recent = true
-
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
-  }
-
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-
-  owners = ["amazon"] # Canonical
-}
-
-
-
-resource "aws_security_group" "mywebsecurity" {
-  name        = "ownsecurityrules"
-  description = "Allow TLS inbound traffic"
-   vpc_id      = var.vpc_id
- 
-   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-     }
-  ingress {
-    description      = "HTTP"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-     }
-
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-
-  tags = {
-    Name = "${var.env}-sg"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.52.0"
+    }
   }
 }
 
-resource "aws_instance" "web" {
-    //count =2 
-  ami           = data.aws_ami.my-ami.id
-  //instance_type = "t2.micro"
-  instance_type=var.instance_type
-  associate_public_ip_address =true
-   #subnet_id=var.subnet_id
-   vpc_security_group_ids = [aws_security_group.mywebsecurity.id]
-   key_name="ansible"
-  // user_data=file("server-script.sh")
-   
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+  #   access_key = "my-access-key"
+  #   secret_key = "my-secret-key"
+}
+
+data "template_file" "user_data" {
+  template = file("./user-creation.yaml")
+}
+
+resource "aws_key_pair" "sshkey" {
+  public_key = file("./ec2sshkey.pub")
+}
+data "aws_security_group" "launchwizard1" {
+  id = "sg-0a9adc7bd77dd9460"
+}
+resource "aws_instance" "tf2server" {
+  ami           = "ami-067aaeea6813afbde" # ap-south-1
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.sshkey.id
+  vpc_security_group_ids = [data.aws_security_group.launchwizard1.id]
+  user_data	=  data.template_file.user_data.rendered
+
   tags = {
-    Name = "${var.env}-mywebserver"
+    Name = "webserver"
   }
-  
+}
+
+output "ipaddress" {
+  value = aws_instance.tf2server.public_ip
+
+}
+
+output "subnet" {
+  value = aws_instance.tf2server.subnet_id
+}
+
+
+output "sgroup" {
+  value = aws_instance.tf2server.security_groups
 }
